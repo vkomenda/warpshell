cf034046d4d65a57590ded53b14c88c8db281e78
Vladimir Komendantskiy <komendantsky@gmail.com>
2022-03-31 17:05:50 +0100

    added the POH example

---
+++ sw/rust/devices/examples/poh.rs
@@ -0,0 +1,87 @@
+extern crate warp_devices;
+
+mod poh_core;
+
+use poh_core::{DataBaseAddrs, PohCoreOps, PohCoreParam};
+use warp_devices::{
+    varium_c1100::VariumC1100,
+    xdma::{DmaBuffer, XdmaOps},
+};
+
+const HASH_BYTES: usize = 32;
+
+const IN_HASH: [u8; HASH_BYTES] = [
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00,
+    // 0x01, 0xba, 0x47, 0x19, 0xc8, 0x0b, 0x6f, 0xe9, 0x11, 0xb0, 0x91, 0xa7, 0xc0, 0x51, 0x24, 0xb6,
+    // 0x4e, 0xee, 0xce, 0x96, 0x4e, 0x09, 0xc0, 0x58, 0xef, 0x8f, 0x98, 0x05, 0xda, 0xca, 0x54, 0x6b,
+];
+
+const OUT_HASH: [u8; HASH_BYTES] = [
+    0x9c, 0x82, 0x72, 0x01, 0xb9, 0x40, 0x19, 0xb4, 0x2f, 0x85, 0x70, 0x6b, 0xc4, 0x9c, 0x59, 0xff,
+    0x84, 0xb5, 0x60, 0x4d, 0x11, 0xca, 0xaf, 0xb9, 0x0a, 0xb9, 0x48, 0x56, 0xc4, 0xe1, 0xdd, 0x7a,
+];
+
+impl PohCoreParam for VariumC1100 {
+    const BASE_ADDR: u64 = 0x0005_0000;
+}
+
+fn main() {
+    env_logger::init();
+
+    let varium = VariumC1100::new().expect("cannot construct device");
+
+    // let mut hashes_buffer = DmaBuffer::new(HASH_BYTES);
+    // let mut num_iters_buffer = DmaBuffer::new(8);
+
+    // hashes_buffer.get_mut().extend_from_slice(&IN_HASH);
+    // num_iters_buffer
+    //     .get_mut()
+    //     .extend_from_slice(&1u64.to_le_bytes());
+
+    // let addrs = DataBaseAddrs {
+    //     in_hashes_base: 0,
+    //     num_iters_base: 4096,
+    //     out_hashes_base: 8192,
+    // };
+
+    // varium.init_poh(addrs, 1).expect("init");
+
+    // // Write the inputs to the card.
+    // varium
+    //     .dma_write(&hashes_buffer, addrs.in_hashes_base)
+    //     .expect("write hashes");
+    // varium
+    //     .dma_write(&num_iters_buffer, addrs.num_iters_base)
+    //     .expect("write num_iters");
+
+    // varium.run_poh().expect("run");
+
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+    // varium
+    //     .dma_read(&mut hashes_buffer, addrs.out_hashes_base)
+    //     .expect("read hashes");
+
+    // let hash = &hashes_buffer.as_slice()[0..HASH_BYTES];
+    // println!("output hash {}", hex::encode(hash));
+    // assert_eq!(hash, OUT_HASH);
+
+    let mut buffer = DmaBuffer::new(12288);
+    for i in 0..12288 {
+        buffer.get_mut().push(1);
+    }
+    varium.dma_write(&buffer, 0).expect("write mem");
+    for i in 0..12288 {
+        buffer.get_mut().push(0);
+    }
+    varium.dma_read(&mut buffer, 0).expect("read mem");
+
+    let mem = &buffer.as_slice()[0..12288];
+    println!("mem {}", hex::encode(mem));
+
+    // let x: u8 = rand::random();
+    // let result = varium.compute(x as u32).expect("cannot compute result");
+    // println!("0x{:X}^2 = 0x{:X}", x, result);
+    // assert_eq!(result, x as u32 * x as u32);
+}
---
+++ sw/rust/devices/examples/poh_core.rs
@@ -0,0 +1,128 @@
+use enum_iterator_derive::IntoEnumIterator;
+use warp_devices::xdma::{Error as XdmaError, XdmaOps};
+
+#[repr(u32)]
+enum ControlRegBit {
+    Start = 0b0001, // (Read/Write/COH)
+    Done = 0b0010,  // (Read/COR)
+    Idle = 0b0100,  // (Read)
+                    // Ready = 0b1000,            // (Read)
+                    // AutoRestart = 0x1000_0000, // (Read/Write)
+}
+
+#[derive(Copy, Clone, Debug, IntoEnumIterator, PartialEq)]
+#[repr(u64)]
+enum PohCoreReg {
+    Control = 0,
+    GlobalInterruptEnable = 0x04,
+    IpInterruptEnable = 0x08,
+    IpInterruptStatus = 0x0c,
+    InHashesLow = 0x10,
+    InHashesHigh = 0x14,
+    NumItersLow = 0x1c,
+    NumItersHigh = 0x20,
+    NumHashes = 0x28,
+    OutHashesLow = 0x30,
+    OutHashesHigh = 0x34,
+}
+
+/// POH (Vitis HLS) core parameters
+pub trait PohCoreParam {
+    const BASE_ADDR: u64;
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    XdmaFailed(XdmaError),
+}
+
+impl From<XdmaError> for Error {
+    fn from(e: XdmaError) -> Self {
+        Self::XdmaFailed(e)
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct DataBaseAddrs {
+    pub in_hashes_base: u64,
+    pub num_iters_base: u64,
+    pub out_hashes_base: u64,
+}
+
+pub trait PohCoreOps {
+    /// Initialises the POH core.
+    fn init_poh(&self, base_addrs: DataBaseAddrs, num_hashes: u32) -> Result<()>;
+    /// Starts computing the hashes and waits until the POH core reaches the DONE state.
+    fn run_poh(&self) -> Result<()>;
+}
+
+impl<T> PohCoreOps for T
+where
+    T: XdmaOps + PohCoreParam,
+{
+    fn init_poh(&self, base_addrs: DataBaseAddrs, num_hashes: u32) -> Result<()> {
+        let mut control_reg = 0;
+        let mut control_bytes = [0u8; 4];
+
+        // Wait for IDLE.
+        while control_reg & ControlRegBit::Idle as u32 != ControlRegBit::Idle as u32 {
+            self.shell_read(&mut control_bytes, T::BASE_ADDR)?;
+            control_reg = u32::from_le_bytes(control_bytes);
+        }
+
+        // Write the inputs.
+        let in_hashes_bytes = base_addrs.in_hashes_base.to_le_bytes();
+        self.shell_write(
+            &in_hashes_bytes[0..4],
+            T::BASE_ADDR + PohCoreReg::InHashesLow as u64,
+        )?;
+        self.shell_write(
+            &in_hashes_bytes[4..8],
+            T::BASE_ADDR + PohCoreReg::InHashesHigh as u64,
+        )?;
+        let num_iters_bytes = base_addrs.num_iters_base.to_le_bytes();
+        self.shell_write(
+            &num_iters_bytes[0..4],
+            T::BASE_ADDR + PohCoreReg::NumItersLow as u64,
+        )?;
+        self.shell_write(
+            &num_iters_bytes[4..8],
+            T::BASE_ADDR + PohCoreReg::NumItersHigh as u64,
+        )?;
+        let num_hashes_bytes = num_hashes.to_le_bytes();
+        self.shell_write(
+            &num_hashes_bytes,
+            T::BASE_ADDR + PohCoreReg::NumHashes as u64,
+        )?;
+        let out_hashes_bytes = base_addrs.out_hashes_base.to_le_bytes();
+        self.shell_write(
+            &out_hashes_bytes[0..4],
+            T::BASE_ADDR + PohCoreReg::OutHashesLow as u64,
+        )?;
+        self.shell_write(
+            &out_hashes_bytes[4..8],
+            T::BASE_ADDR + PohCoreReg::OutHashesHigh as u64,
+        )?;
+
+        Ok(())
+    }
+
+    fn run_poh(&self) -> Result<()> {
+        let mut control_reg = 0;
+        let mut control_bytes = [0u8; 4];
+
+        // Send the start command.
+        let start_cmd = (ControlRegBit::Start as u32).to_le_bytes();
+        self.shell_write(&start_cmd, T::BASE_ADDR)?;
+
+        // Wait until DONE.
+        while control_reg & ControlRegBit::Done as u32 != ControlRegBit::Done as u32 {
+            self.shell_read(&mut control_bytes, T::BASE_ADDR)?;
+            control_reg = u32::from_le_bytes(control_bytes);
+        }
+
+        Ok(())
+    }
+}
